690c2c0e3e1fe5eb80fd6591f86445c94e6cc447
chengyachong <chengyachong@126.com>
2021-11-17 23:44:19 +0800

    函数式接口function-andthen

---
+++ spring-boot-web5/src/main/java/com/cyc/springboot/test/FunctionAndThenTest.java
@@ -0,0 +1,18 @@
+package com.cyc.springboot.test;
+
+import java.util.function.Function;
+
+public class FunctionAndThenTest {
+    public static void main(String[] args) {
+          test(mag ->{
+              return Integer.parseInt(mag);
+          },msg2->{
+              return msg2*10;
+          });
+    }
+   public static void test(Function<String ,Integer> f1 , Function<Integer,Integer> f2) {
+        Integer i1= f1.apply("6666");
+        Integer i2 = f2.apply(i1);
+       System.out.println(i2);
+   }
+}
