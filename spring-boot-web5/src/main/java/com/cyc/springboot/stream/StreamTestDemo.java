9e2ee0d0cbc35040ec56ad8d2f5e2ba439a868c3
chengyachong <chengyachong@126.com>
2021-11-21 14:51:36 +0800

    stream流

---
+++ spring-boot-web5/src/main/java/com/cyc/springboot/stream/StreamTestDemo.java
@@ -0,0 +1,12 @@
+package com.cyc.springboot.stream;
+
+public class StreamTestDemo {
+    /*** 1. 第一个队伍只保留姓名长度为3的成员 * 2. 第一个队伍筛选之后只要前3个人 * 3. 第二个队伍只要姓张的成员 * 4. 第二个队伍筛选之后不要前两个人 * 5. 将两个队伍合并为一个队伍 * 6. 根据姓名创建Person对象
+     输出结果：
+     5.Stream结果收集
+     5.1 结果收集到集合中
+     * 7. 打印整个队伍的Person信息 * @param args */
+    public static void main(String[] args) {
+
+    }
+}
